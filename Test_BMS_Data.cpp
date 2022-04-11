cbb3ed6448d4e7e88ad1f8125326f3c2f6deaccf
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-04-11 22:49:04 +0530

    Update Test_BMS_Data.cpp

--- Test_BMS_Data.cpp
+++ Test_BMS_Data.cpp
@@ -21,8 +21,8 @@ TEST_CASE(" Test case to check if user request to halt the read after (100-1) as
 {
  /* Note: haltBmsRead.txt contains value as 0 in this case */
  /* This will stream only 100 datas */
- int runTimeIpNum_u32 = 100; /* Every 100th count of loop will enter into user request check to Halt the read or not */
- int maxStreamingRange = 100; /* this input will be taken as multiples of 5 */
+ int runTimeIpNum_u32 = 50; /* Every 100th count of loop will enter into user request check to Halt the read or not */
+ int maxStreamingRange = 50; /* this input will be taken as multiples of 5 */
  REQUIRE(dataBmsMain((runTimeIpNum_u32-1),maxStreamingRange) == OK_STATUS);
 }
 
